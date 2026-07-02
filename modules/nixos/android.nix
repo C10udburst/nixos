ba286c46fd007996a9f3da838e299bb788dfbef6
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2026-07-02 20:20:27 +0200

    "31"

--- modules/nixos/android.nix
+++ modules/nixos/android.nix
@@ -11,11 +11,14 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    nixpkgs.config.android_sdk.accept_license = true;
+
     environment.systemPackages = with pkgs; [
       scrcpy
       android-tools
       jmtpfs
       android-file-transfer
+      androidenv.androidPkgs.androidsdk
     ];
   };
 }
