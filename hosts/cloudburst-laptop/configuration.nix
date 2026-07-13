85aa8c29e6e12cbeb1e22c8ef5c27577547356b7
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2026-07-13 13:27:30 +0200

    cloudburst-laptop:"15"

--- hosts/cloudburst-laptop/configuration.nix
+++ hosts/cloudburst-laptop/configuration.nix
@@ -19,17 +19,6 @@ in {
   nixpkgs.overlays = [
     (final: prev: {
       driftwm = inputs.driftwm.packages.${prev.stdenv.hostPlatform.system}.default;
-      linuxPackages = prev.linuxPackages.extend (lfinal: lprev: {
-        msi-ec = lprev.msi-ec.overrideAttrs (oldAttrs: {
-          postPatch =
-            (oldAttrs.postPatch or "")
-            + ''
-                  substituteInPlace msi-ec.c \
-                    --replace '"17E8EMS1.101",' '"17E8EMS1.101",
-              "16U7EMS1.10C",'
-            '';
-        });
-      });
     })
   ];
 
@@ -41,7 +30,10 @@ in {
   # MSI Embedded Controller (EC) support
   boot.extraModulePackages = [config.boot.kernelPackages.msi-ec];
   boot.kernelModules = ["msi-ec"];
-  boot.kernelParams = ["ec_sys.write_support=1"];
+  boot.kernelParams = [
+    "ec_sys.write_support=1"
+    "msi-ec.firmware=16U7EMS1.106"
+  ];
 
   networking.hostName = "cloudburst-laptop";
   networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
