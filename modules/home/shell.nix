8ac1ba04d15a377d5b3071c012f0d7a37eca0253
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2026-07-15 19:52:27 +0200

    cloudburst-desktop:"163"

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -31,7 +31,6 @@ in {
         pause = "printf \"Press any key to continue...\"; read -s -n 1; printf \"\\n\"";
         "cd.." = "cd ..";
         "-" = "cd -";
-        strace-pick = "kitty sudo strace -p $(ps aux | awk '{print $2, $11}' | noctalia-dmenu | awk '{print $1}') -e trace=read -s 10000";
         icat = "kitty +kitten icat";
       }
       // (lib.listToAttrs (
