da15ea080710281293ded46776c3a561e859dfe0
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2026-07-15 21:20:32 +0200

    cloudburst-desktop:"168"

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -19,7 +19,6 @@ in {
         cls = "printf '\033[2J\033[3J\033[1;1H'";
         ll = "eza -la --sort name --group-directories-first --git --smart-group -h --extended --icons -M";
         l = "eza --icons --git --group-directories-first";
-        ls = "eza --icons --git --group-directories-first";
         tree = "eza --icons --git --group-directories-first -T -L 2";
         t = "eza --icons --git --group-directories-first -T -L 2";
         # "top-10" = "history | awk '{CMD[\$2]++;count++;}END { for (a in CMD)print CMD[a] \" \" CMD[a]/count*100 \"% \" a;}' | grep -v \"./\" | column -c3 -s \" \" -t | sort -nr | nl |  head -n10";
