aa29c8d604e43378957d55f46909d53ef578446b
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2026-09-03 22:15:38 +0200

    cloudburst-laptop:55 cloudburst-desktop:219

--- modules/home/threed.nix
+++ modules/home/threed.nix
@@ -1,19 +1,26 @@
 {
   config,
   lib,
+  pkgs,
   inputs,
   ...
 }: let
   cfg = config.homeSettings.threed;
+  blenderVersion = lib.versions.majorMinor pkgs.blender.version;
 in {
   options.homeSettings.threed = lib.mkOption {
     type = lib.types.bool;
     default = false;
-    description = "Enable 3D home-manager configuration (OpenSCAD libraries etc.)";
+    description = "Enable 3D home-manager configuration (OpenSCAD libraries, Blender Stylix theme etc.)";
   };
 
   config = {
     stylix.targets.blender.enable = cfg;
+
+    xdg.configFile = lib.mkIf cfg {
+      "blender/${blenderVersion}/scripts/presets/interface_theme/Stylix.xml".source =
+        config.xdg.configFile."blender/4.5/scripts/presets/interface_theme/Stylix.xml".source;
+    };
 
     xdg.dataFile = lib.mkIf cfg {
       "OpenSCAD/libraries/BOSL2".source = inputs.openscad-bosl2;
